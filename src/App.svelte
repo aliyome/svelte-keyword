6e325adeb267d34dea44607e5f5bdd4a0d5933e6
aliyome <aliyome@gmail.com>
2020-12-27 16:53:21 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<main class="grid mx-auto max-w-screen-xl p-6 text-gray-800">
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
